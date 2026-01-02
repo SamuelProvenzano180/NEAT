fa058a0e99d77c24e9e1753ba728f4b0926a5da1
Samuel Provenzano <samuelprovenzano@Samuels-MacBook-Pro.local>
2026-01-01 21:04:28 -0500

    Restore missing extension and script files

--- SConstruct.py
+++ SConstruct.py
@@ -4,12 +4,11 @@ import sys
 env = SConscript("godot-cpp/SConstruct")
 
 env.Append(CPPPATH=["src/"])
-sources = Glob("src/*.cpp") # <--- Captures both NEATAgent.cpp and NetworkAgent.cpp
+sources = Glob("src/*.cpp")
 
 if env["platform"] == "macos":
-    # Creates bin/NEAT.macos.debug.framework/...
     library = env.SharedLibrary(
-        "demoproject/bin/NEAT.{}.{}.framework/NEAT.{}.{}".format(
+        "addons/NEAT/bin/NEAT.{}.{}.framework/NEAT.{}.{}".format(
             env["platform"], env["target"], env["platform"], env["target"]
         ),
         source=sources,
@@ -18,7 +17,7 @@ if env["platform"] == "macos":
     env.Append(LINKFLAGS=["-framework", "Accelerate"])
 else:
     library = env.SharedLibrary(
-        "demoproject/bin/NEAT{}{}".format(env["suffix"], env["SHLIBSUFFIX"]),
+        "addons/NEAT/bin/libNEAT{}{}".format(env["suffix"], env["SHLIBSUFFIX"]),
         source=sources,
     )
 
---
+++ addons/NEAT/NEAT.gd
@@ -0,0 +1,8 @@
+@tool
+extends EditorPlugin
+
+func _enter_tree():
+	print("NEAT GDExtension plugin loaded.")
+
+func _exit_tree():
+	pass
